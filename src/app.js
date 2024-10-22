72efeff16e365c9f0aab05f9231e6bc90729cae2
Thomas Psik <thomas.psik@htlwienwest.at>
2024-10-22 22:41:51 +0200

    update Okt.2024

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import path from 'path';
+import morgan from "morgan";
 
 import testRoute from './api/routes/test.js';
 
@@ -9,6 +10,7 @@ const dirname = path.resolve();
 const app = express();
 
 app.use(cors());
+app.use(morgan('dev'));
 
 app.use(express.static(path.join(dirname, '/public')));
 app.use(express.json());
